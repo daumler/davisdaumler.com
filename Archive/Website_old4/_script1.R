48a4777882a22cc40deef931a7847c72fa8c6424
Davis <58414686+daumler@users.noreply.github.com>
2024-07-17 12:33:33 -0400

    Removing archive

--- Archive/Website_old4/_script1.R
+++
@@ -1,5 +0,0 @@
-# To build the website, open the RProject file website.Rproj in RStudio and type
-blogdown::hugo_build(local=TRUE)
-
-# To preview the website, type in RStudio
-blogdown::serve_site()
